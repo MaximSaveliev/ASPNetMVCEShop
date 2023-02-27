8842ef19285cdb5b7f2dd75311c0c265742ac9a8
MaximSaveliev <maxim.saveliev@mail.ru>
2023-02-27 18:50:53 +0200

    Add project files.

---
+++ ASPNetMVCEShopBE/App_Start/BundelConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace ASPNetMVCEShopBE.App_Start
+{
+    public static class BundelConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
+                "~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/jqueryui/css").Include(
+                "~/Content/themes/base/jquery-ui.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/util/css").Include(
+                "~/Vendor/util.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/main/css").Include(
+                "~/Vendor/main.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/animate/css").Include(
+                "~/Vendor/main.css", new CssRewriteUrlTransform()));
+        }
+    }
+}
